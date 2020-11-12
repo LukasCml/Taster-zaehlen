bbbcdca8f84e2ac041d42543b691dc06077677c5
LukasCml <lukas.camehl@gmx.de>
2020-11-12 15:47:08 +0100

    Version Fertig

--- Taster_zaehlen/Taster_zaehlen.ino
+++ Taster_zaehlen/Taster_zaehlen.ino
@@ -1,46 +1,71 @@
-/*
-   Programm:          Taster Zählen
-   Letzte Änderung:   11.11.20
-   Programmierer:     LC
-
-   Hardware:      Arduino UNO / Taster 1 an Pin 11 , Taster 2 an Pin 12 / Beide Pull-Down
-*/
-
-#include <OneButton.h>
-
-OneButton button1(1, false);                             // Taster 1 erstellen und dem Pin 11 zuweisen
-OneButton button2(2, false);                             // Taster 2 erstellen und dem Pin 12 zuweisen
-
-int wert = 0;
-
-
-void setup()
-{
-  Serial.begin(9600);
-  button1.attachClick(taster1);                           // Sobald Taster 1 gedrückt wird, wird die Funktion taster1() ausgeführt
-  button2.attachClick(taster2);                           // Sobald Taster 2 gedrückt wird, wird die Funktion taster2() ausgeführt
-}
-
-void loop()
-{
-  button1.tick();                                         // Erfassung des Taster1-Klick
-  button2.tick();                                         // Erfassung des Taster2-Klick
-  delay(10);
-}
-
-
-void taster1()
-{
-  wert++;                                                 // Sobald der Taster 1 gdrückt wurde, wird die variable "wert" um 1 addiert
-  Serial.print("Zähler: ");                               // Gleichzeitig wird der Zählerstand mithilfe der variable "wert" im seriellen Monitor angezeigt
-  Serial.println(wert);
-}
-
-
-
-void taster2()
-{
-  wert--;                                                 // Sobald der Taster 2 gdrückt wurde, wird die variable "wert" um 1 subtrahiert
-  Serial.print("Zähler: ");                               // Gleichzeitig wird der Zählerstand mithilfe der variable "wert" im seriellen Monitor angezeigt
-  Serial.println(wert);
-}
+    /*
+       Programm:          Taster Zählen
+       Letzte Änderung:   11.11.20
+       Programmierer:     LC
+       Version:           1.0
+       Bibliothek:        OneButton
+    
+       Hardware:      Arduino UNO / Taster 1 an Pin 11 , Taster 2 an Pin 12 / Beide Pull-Down
+    */
+    
+    #include <OneButton.h>
+    
+    OneButton button1(2, false);                           // Taster 1 erstellen und dem Pin 11 zuweisen
+    OneButton button2(3, false);                           // Taster 2 erstellen und dem Pin 12 zuweisen
+    
+    byte led[8] = {6, 7, 8, 9, 10, 11, 12, 13};
+    
+    int wert = 0;
+    int index = -1;
+    
+    bool taster1Gedrueckt = false;
+    bool taster2Gedrueckt = false;
+    
+    void setup()
+    {
+      Serial.begin(9600);
+      button1.attachClick(Taster1);                       // Sobald Taster 1 gedrückt wurde wird die Funktion Taster 1 ausgeführt
+      button2.attachClick(Taster2);                       // Sobald Taster 2 gedrückt wurde wird die Funktion Taster 2 ausgeführt
+      for (byte i = 0; i < 8; i++)                        // Ausgänge der LED's werden zugewiesen
+      {
+        pinMode (led[i], OUTPUT);
+      }
+    }
+    
+    void loop()
+    {
+      button1.tick();                                     // Abfrage wann Taster 1 gedrückt wird
+      button2.tick();                                     // Abfrage wann Taster 2 gedrückt wird
+      delay(10);
+      if (taster1Gedrueckt == true)                       // Schleife wird ausgeführt wenn Taster 1 gedrückt wurde
+      {
+        digitalWrite(led[index], LOW);                    // In der Schleife soll zuerst der aktuelle Index auf LOW gesetzt                  
+        wert++;                                           // Dann wird der Index/Wert um 1 erhöht 
+        index++;
+        Serial.print("Zähler: ");
+        Serial.println(wert);
+        digitalWrite(led[index], HIGH);                   // Anschließend wir dann der verminderte Index auf HIGH gesetzt
+        taster1Gedrueckt = false;                         // Merker wird wieder auf "false" gesetzt, damit die Schleife nur einmal durchläuft
+      }
+      else if (taster2Gedrueckt == true)                  // Schleife wird ausgeführt wenn Taster 2 gedrückt wurde
+      {
+        digitalWrite(led[index], LOW);                    // In der Schleife soll zuerst der aktuelle Index auf LOW gesetzt
+        wert--;                                           // Dann wird der Index/Wert um 1 vermindert
+        index--;
+        Serial.print("Zähler: ");
+        Serial.println(wert);
+        digitalWrite(led[index], HIGH);                   // Anschließend wir dann der verminderte Index auf HIGH gesetzt
+        taster2Gedrueckt = false;                         // Merker wird wieder auf "false" gesetzt, damit die Schleife nur einmal durchläuft
+      }
+      
+    }
+    
+    void Taster1()                                        // Funktion wird ausgeführt wenn Taster 1 gedrückt wurde
+    {
+      taster1Gedrueckt = !taster1Gedrueckt;               // Merker, dass Taster 1 gedrückt wurde wird negiert, damit
+    }                                                     // Schleife ausgeführt wird
+    
+    void Taster2()                                        // Funktion wird ausgeführt wenn Taster 2 gedrückt wurde
+    {
+      taster2Gedrueckt = !taster2Gedrueckt;               // Merker, dass Taster 2 gedrückt wurde wird negiert, damit
+    }                                                     // Schleife ausgeführt wird

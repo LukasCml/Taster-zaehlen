f940de4c691ee16aa487660031e0da6da3d78a77
LukasCml <71316048+LukasCml@users.noreply.github.com>
2020-11-12 10:02:25 +0100

    Version 4

--- Taster_zaehlen/Taster_zaehlen.ino
+++ Taster_zaehlen/Taster_zaehlen.ino
@@ -17,22 +17,22 @@ int wert = 0;
 void setup()
 {
   Serial.begin(9600);
-  button1.attachClick(taster1);
-  button2.attachClick(taster2);
+  button1.attachClick(taster1);                           // Sobald Taster 1 gedrückt wird, wird die Funktion taster1() ausgeführt
+  button2.attachClick(taster2);                           // Sobald Taster 2 gedrückt wird, wird die Funktion taster2() ausgeführt
 }
 
 void loop()
 {
-  button1.tick();
-  button2.tick();
+  button1.tick();                                         // Erfassung des Taster1-Klick
+  button2.tick();                                         // Erfassung des Taster2-Klick
   delay(10);
 }
 
 
 void taster1()
 {
-  wert++;
-  Serial.print("Zähler: ");
+  wert++;                                                 // Sobald der Taster 1 gdrückt wurde, wird die variable "wert" um 1 addiert
+  Serial.print("Zähler: ");                               // Gleichzeitig wird der Zählerstand mithilfe der variable "wert" im seriellen Monitor angezeigt
   Serial.println(wert);
 }
 
@@ -40,7 +40,7 @@ void taster1()
 
 void taster2()
 {
-  wert--;
-  Serial.print("Zähler: ");
+  wert--;                                                 // Sobald der Taster 2 gdrückt wurde, wird die variable "wert" um 1 subtrahiert
+  Serial.print("Zähler: ");                               // Gleichzeitig wird der Zählerstand mithilfe der variable "wert" im seriellen Monitor angezeigt
   Serial.println(wert);
 }
